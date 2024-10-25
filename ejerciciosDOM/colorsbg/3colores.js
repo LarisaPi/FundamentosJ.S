b74f9451b8df71e4874affcf0b81c796533fef8c
larisap <laripinacortez@gmail.com>
2024-10-24 21:01:20 -0600

    04 Modificación

--- ejerciciosDOM/colorsbg/3colores.js
+++
@@ -1,9 +0,0 @@
-
-function nuevoColor() {
-    const Rojo = document.getElementById('Rojo').value;
-    const Verde = document.getElementById('Verde').value;
-    const Azul = document.getElementById('Azul').value;
-    const Transparencia = document.getElementById('Transparencia').value;
-
-    document.body.style.backgroundColor = `rgb(${Rojo}, ${Verde}, ${Azul}, ${Transparencia})`;
-}
